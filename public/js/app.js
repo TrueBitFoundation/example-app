4299ad79d6b7a8f00c65a49e19ea84cf73bfd96d
hswick <hdswick@gmail.com>
2018-10-01 13:38:51 -0500

    Detecting metamask

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,25 @@
+function showJSScrypt(hash) {
+    return "<div>Scrypt Hash from js-scrypt:</div> <div>" + hash + "</div>"
+}
+
+function showTruebitScrypt(hash) {
+    return "<div>Scrypt Hash from TrueBit solver:</div> <div>" + hash + "</div>"
+}
+
 function runScrypt() {
     data = document.getElementById('input-data').value
     hash = s.crypto_scrypt(data, "foo", 1024, 1, 1, 256)
-    document.getElementById('js-scrypt').innerHTML = s.to_hex(hash)
+    document.getElementById('js-scrypt').innerHTML = showJSScrypt(s.to_hex(hash))
+
+    document.getElementById('tb-scrypt').innerHTML = showTruebitScrypt("foobar")
 }
+
+function init() {
+    const isMetaMaskEnabled = function() { return !!window.web3 }
+
+    if (!isMetaMaskEnabled()) {
+	document.getElementById('app').innerHTML = "Please install MetaMask"
+    }    
+}
+
+window.onload = init
