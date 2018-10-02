3e6659b8e938037dbb7ffea6929b2454a3b66f80
hswick <hdswick@gmail.com>
2018-10-02 11:44:48 -0500

    Deploy script working now

--- deploy.js
+++ deploy.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 
-let abi = fs.readFileSync('./build/Scrypt.abi')
+const ipfs = require('ipfs-api')("localhost", '5001', {protocol: 'http'})
+
+let abi = JSON.parse(fs.readFileSync('./build/Scrypt.abi'))
 let bin = fs.readFileSync('./build/Scrypt.bin')
 
 let info = JSON.parse(fs.readFileSync('./scrypt-task/info.json'))
@@ -10,25 +12,38 @@ const host = "http://localhost:8545"
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider(host))
 
-let contract = new web3.eth.Contract(abi)
+async function deploy() {
 
-(async function () {
+    //Upload file to IPFS
+    let codeBuf = fs.readFileSync("./scrypt-task/task.wasm")
+
+    let ipfsHash = (await ipfs.files.add([{content: codeBuf, path: "task.wasm"}]))[0].hash
+
+    if (ipfsHash == info.ipfsHash) {
+	throw "Wrong IPFS Hashes"
+    }
+
+    //Deploy contract with appropriate artifacts
 
     let networkName = await web3.eth.net.getNetworkType()
 
     let artifacts = JSON.parse(fs.readFileSync('./truebit-os/wasm-client/' + networkName + '.json'))
 
-    let accounts = await web3.getAccounts()
+    let accounts = await web3.eth.getAccounts()
 
-    let options = {from: accounts[0], gas: 2000000}
+    let options = {from: accounts[0].toLowerCase(), gas: 2000000}
 
     let args = [
-	artifacts.incentiveLayer.address,
-	artifacts.tru.address,
-	artifacts.fileSystem.address,
-	info.ipfsHash,
-	info.codeHash
+	artifacts.incentiveLayer.address.toLowerCase(),
+	artifacts.tru.address.toLowerCase(),
+	artifacts.fileSystem.address.toLowerCase(),
+	info.ipfshash,
+	info.codehash
     ]
+
+    let contract = new web3.eth.Contract(abi)
     
     await contract.deploy({data: "0x" + bin, arguments: args}).send(options)
-})()
+}
+
+deploy()
